dc26ec5e12593e37140ae064b2b7a0da6bba71b6
mustran <45851256+mustran@users.noreply.github.com>
2019-09-04 22:15:02 +0200

    Update Relations.java

--- Relations.java
+++ Relations.java
@@ -1,1 +1,8 @@
+/* 
+Grandfather-nephew Relations Problem 11 (3 / 7)
+You are given a binary tree. You need to find the number of grandfather-nephew realtions. Grandfather-nephew relations exists, if one of the children of A, is a parent to B. Recursion is not allowed.
+
+Class name (Java): Relations.
+*/
+
 
